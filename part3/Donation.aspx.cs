715778765e05787c8436a6b1db4fd53570fac4bb
Erin R Martin <erinrmartin@yahoo.com>
2017-05-01 22:42:51 -0700

    part 3 of hw assingment

---
+++ part3/Donation.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Session["userKey"]==null)
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        Community_AssistEntities db = new Community_AssistEntities();
+        Donor a = new Donor();
+        a.DonorName = DonateTextBox.Text;
+
+        db.Donors.Add(a);
+        db.SaveChanges();
+
+        Response.Redirect("DonationList.aspx");
+    }
+}
